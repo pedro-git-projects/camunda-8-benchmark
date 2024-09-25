c76951a96a3851f532de702fe692d737628a7ae9
X333668 <pedro.pereira@prservicos.com.br>
2024-09-24 23:56:48 -0300

    adding benchmark capabilities

--- spring-zeebe-mvc/src/main/java/dev/nilptr/springzeebemvc/workers/ProcessPaymentWorker.java
+++ spring-zeebe-mvc/src/main/java/dev/nilptr/springzeebemvc/workers/ProcessPaymentWorker.java
@@ -1,14 +1,26 @@
 package dev.nilptr.springzeebemvc.workers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.nilptr.springzeebemvc.dtos.PlaceOrderDto;
 import dev.nilptr.springzeebemvc.services.ProcessPaymentService;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
 import io.camunda.zeebe.spring.client.annotation.VariablesAsType;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -16,8 +28,20 @@ import org.springframework.stereotype.Service;
 public class ProcessPaymentWorker {
     private final ProcessPaymentService processPaymentService;
 
+    private final int targetJobCount = 10;
+    private int jobCompletionCount = 0;
+    private long startTime = 0;
+    private final List<Map<String, Object>> throughputData = new LinkedList<>();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @JobWorker(type = "processPayment", autoComplete = false)
     public void handleProcessPayment(JobClient client, ActivatedJob job, @VariablesAsType PlaceOrderDto placeOrderDto) {
+        long jobStartTime = System.currentTimeMillis();
+
+        if (jobCompletionCount == 0) {
+            startTime = jobStartTime;
+        }
+
         log.info("Received variables: " + placeOrderDto.toString());
 
         processPaymentService.processPayment(placeOrderDto)
@@ -31,7 +55,13 @@ public class ProcessPaymentWorker {
                             .toCompletableFuture();
                 })
                 .thenAccept(response -> {
+                    jobCompletionCount++;
                     log.info("Successfully completed job for order: " + placeOrderDto.getOrderId());
+                    if (jobCompletionCount >= targetJobCount) {
+                        long endTime = System.currentTimeMillis();
+                        calculateThroughput(endTime);
+                        jobCompletionCount = 0; // resetar para o próximo batch
+                    }
                 })
                 .exceptionally(throwable -> {
                     log.error("Failed to complete job: " + throwable.getMessage());
@@ -48,5 +78,45 @@ public class ProcessPaymentWorker {
                 });
 
         log.info("Processing payment for order: " + placeOrderDto.getOrderId());
+    }
+
+    private void calculateThroughput(long endTime) {
+        long totalTimeMillis = endTime - startTime;
+        double totalTimeSeconds = totalTimeMillis / 1000.0;
+
+        // Throughput: n jobs completed per second
+        double jobsPerSecond = targetJobCount / totalTimeSeconds;
+        log.info("Throughput: {} jobs per second for the last {} jobs", jobsPerSecond, targetJobCount);
+
+        Map<String, Object> throughputEntry = new HashMap<>();
+        throughputEntry.put("timestamp", endTime);
+        throughputEntry.put("throughput", jobsPerSecond);
+        throughputData.add(throughputEntry);
+
+        dumpThroughputDataToFile();
+    }
+
+
+    private void dumpThroughputDataToFile() {
+        File file = new File("throughput-data-batch.json");
+
+        List<Map<String, Object>> existingData = new LinkedList<>();
+        if (file.exists()) {
+            try {
+                existingData = objectMapper.readValue(file, List.class);
+            } catch (IOException e) {
+                log.error("Failed to read existing throughput data from file ", e);
+            }
+        }
+
+        existingData.addAll(throughputData);
+
+        try {
+            objectMapper.writeValue(file, existingData);
+            log.info("Throughput data appended to file: throughput-data-batch.json");
+        } catch (IOException e) {
+            log.error("Failed to write throughput data to file", e);
+        }
+        throughputData.clear();
     }
 }
--- spring-zeebe-webflux/src/main/java/dev/nilptr/spring/zeebe/webflux/workers/ProcessPaymentWorker.java
+++ spring-zeebe-webflux/src/main/java/dev/nilptr/spring/zeebe/webflux/workers/ProcessPaymentWorker.java
@@ -1,5 +1,6 @@
 package dev.nilptr.spring.zeebe.webflux.workers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.nilptr.spring.zeebe.webflux.dtos.PlaceOrderDto;
 import dev.nilptr.spring.zeebe.webflux.services.ProcessPaymentService;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
@@ -11,14 +12,33 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class ProcessPaymentWorker {
     private final ProcessPaymentService processPaymentService;
 
+    private final int targetJobCount = 10;
+    private int jobCompletionCount = 0;
+    private long startTime = 0;
+    private final List<Map<String, Object>> throughputData = new LinkedList<>();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @JobWorker(type = "processPayment", autoComplete = false)
     public void handleProcessPayment(JobClient client, ActivatedJob job, @VariablesAsType PlaceOrderDto placeOrderDto) {
+        long jobStartTime = System.currentTimeMillis();
+
+        if (jobCompletionCount == 0) {
+            startTime = jobStartTime;
+        }
+
         log.info("Received variables: " + placeOrderDto.toString());
 
         processPaymentService.processPayment(placeOrderDto)
@@ -32,7 +52,17 @@ public class ProcessPaymentWorker {
                                     .send()
                     );
                 })
-                .doOnSuccess(success -> log.info("Successfully completed job for order: " + placeOrderDto.getOrderId()))
+                .doOnSuccess(success -> {
+                            jobCompletionCount++;
+                            log.info("Successfully completed job for order: " + placeOrderDto.getOrderId());
+
+                            if (jobCompletionCount >= targetJobCount) {
+                                long endTime = System.currentTimeMillis();
+                                calculateThroughput(endTime);
+                                jobCompletionCount = 0;
+                            }
+                        }
+                )
                 .doOnError(throwable -> {
                     log.error("Failed to complete job: " + throwable.getMessage());
                     client.newFailCommand(job.getKey())
@@ -48,5 +78,43 @@ public class ProcessPaymentWorker {
                         success -> log.info("Subscription success for order: " + placeOrderDto.getOrderId()),
                         error -> log.error("Subscription error: " + error.getMessage())
                 );
+    }
+
+    private void calculateThroughput(long endTime) {
+        long totalTimeMillis = endTime - startTime;
+        double totalTimeSeconds = totalTimeMillis / 1000.0;
+        double jobsPerSecond = targetJobCount / totalTimeSeconds;
+
+        log.info("Throughput: {} jobs per second for the last {} jobs", jobsPerSecond, targetJobCount);
+
+        Map<String, Object> throughputEntry = new HashMap<>();
+        throughputEntry.put("timestamp", endTime);
+        throughputEntry.put("throughput", jobsPerSecond);
+        throughputData.add(throughputEntry);
+
+        dumpThroughputDataToFile();
+    }
+
+    private void dumpThroughputDataToFile() {
+        File file = new File("throughput-data-batch.json");
+
+        List<Map<String, Object>> existingData = new LinkedList<>();
+        if (file.exists()) {
+            try {
+                existingData = objectMapper.readValue(file, List.class);
+            } catch (IOException e) {
+                log.error("Failed to read existing throughput data from file ", e);
+            }
+        }
+
+        existingData.addAll(throughputData);
+
+        try {
+            objectMapper.writeValue(file, existingData);
+            log.info("Throughput data appended to file: throughput-data-batch.json");
+        } catch (IOException e) {
+            log.error("Failed to write throughput data to file", e);
+        }
+        throughputData.clear();
     }
 }
